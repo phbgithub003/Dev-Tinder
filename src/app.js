32414a1f89ab9df4c1cfbcd3b38117f864d30b8f
Harshit Bhargava <palacheralaharshit@gmail.com>
2025-10-12 11:54:25 +0530

    working with routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user/:id/:name/:password",(req,res)=>{
+    console.log(req.params);
+    res.send({name:"harshit",age:20});
+})
+
 app.use((req,res)=>{
     res.send("Hello from express");
 })
